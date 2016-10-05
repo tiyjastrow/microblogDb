8e2c67e5a892e9963207ed2906fc24b7b0a9a602
DonatelloDaRuby <collierew@gmail.com>
2016-10-05 16:09:07 -0500

    final commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# microblogDb
-
-## Description
-
-Use Spring Data to store the microblog messages in PostgreSQL. Add editing functionality as well.
---
+++ src/main/java/com/theironyard/MessageRepository.java
@@ -0,0 +1,8 @@
+package com.theironyard;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepository extends CrudRepository<Message, Integer>{
+
+}
